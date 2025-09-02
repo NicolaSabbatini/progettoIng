de9e8a5de00464d8dfe9713c7bec1a4c3328b0ac
Abbo023 <christian.abbondanzieri.ca@gmail.com>
2025-09-02 17:09:22 +0200

    aaaa

--- test/test_contract_controller.py
+++ test/test_contract_controller.py
@@ -1,10 +1,10 @@
 import unittest
 from unittest.mock import MagicMock
-from controllers.contract_controller import ContractController
+from controllers.GestoreContratti import GestoreContratti
 
 class TestContractController(unittest.TestCase):
     def setUp(self):
-        self.controller = ContractController(main_controller=MagicMock())
+        self.controller = GestoreContratti(main_controller=MagicMock())
         # Mock dei modelli
         self.controller.contract_model = MagicMock()
         self.controller.buy_contract_model = MagicMock()
@@ -41,31 +41,6 @@ class TestContractController(unittest.TestCase):
         self.assertFalse(success)
         self.assertEqual(message, "Tutti i campi del contratto sono obbligatori")
     
-    def test_elimina_contratto_success(self):
-        self.controller.contract_model.delete_contract.return_value = True
-        success, message = self.controller.elimina_contratto(1)
-        self.assertTrue(success)
-        self.assertEqual(message, "Contratto eliminato con successo")
-        self.controller.contract_view.refresh_contracts.assert_called_once()
-    
-    def test_elimina_contratto_fail(self):
-        self.controller.contract_model.delete_contract.return_value = False
-        success, message = self.controller.elimina_contratto(1)
-        self.assertFalse(success)
-        self.assertEqual(message, "Errore durante l'eliminazione del contratto")
-    
-    def test_delete_contract_and_fatture_success(self):
-        self.controller.contract_model.delete_contract.return_value = True
-        success, message = self.controller.delete_contract_and_fatture(1, 2)
-        self.assertTrue(success)
-        self.assertEqual(message, "Contratto e fatture collegate eliminate con successo")
-        self.controller.contract_view.refresh_contracts.assert_called_once()
-    
-    def test_delete_contract_and_fatture_fail(self):
-        self.controller.contract_model.delete_contract.return_value = False
-        success, message = self.controller.delete_contract_and_fatture(1, 2)
-        self.assertFalse(success)
-        self.assertEqual(message, "Errore durante l'eliminazione del contratto e delle fatture collegate")
 
 if __name__ == "__main__":
     unittest.main()
