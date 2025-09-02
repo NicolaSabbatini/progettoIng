aca047ad3063587081fe5a0731f378a7d0b40e85
Abbo023 <christian.abbondanzieri.ca@gmail.com>
2025-09-02 17:51:57 +0200

    porcoddio

--- views/LoginView.py
+++ views/LoginView.py
@@ -10,6 +10,71 @@ class LoginView(QWidget):
         self.register_view = None
         self.dashboard_view = None
         self.init_ui()
+    
+    def init_ui(self):
+        self.setWindowTitle('Sistema di Login')
+        #self.setFixedSize(500, 450)
+        
+        
+        # Layout principale
+        main_layout = QVBoxLayout()
+        main_layout.setSpacing(20)
+        main_layout.setContentsMargins(40, 40, 40, 40)
+        
+        # Titolo
+        title = QLabel('Accedi al tuo account')
+        title.setObjectName('title')
+        title.setAlignment(Qt.AlignCenter)
+        main_layout.addWidget(title)
+        
+        # Frame per il form
+        form_frame = QFrame()
+        form_frame.setObjectName('form_frame')
+        form_layout = QVBoxLayout(form_frame)
+        form_layout.setSpacing(15)
+        
+        # Campo username
+        username_label = QLabel('Username:')
+        self.username_input = QLineEdit()
+        self.username_input.setPlaceholderText('Inserisci il tuo username')
+        form_layout.addWidget(username_label)
+        form_layout.addWidget(self.username_input)
+        
+        # Campo password
+        password_label = QLabel('Password:')
+        self.password_input = QLineEdit()
+        self.password_input.setEchoMode(QLineEdit.Password)
+        self.password_input.setPlaceholderText('Inserisci la tua password')
+        form_layout.addWidget(password_label)
+        form_layout.addWidget(self.password_input)
+        
+        # Bottone login
+        self.login_button = QPushButton('Accedi')
+        self.login_button.setObjectName('primary_button')
+        self.login_button.clicked.connect(lambda: self.controller.handle_login(self.username_input.text(), self.password_input.text(), self))
+        #self.login_button.clicked.connect(self.handle_login)
+        form_layout.addWidget(self.login_button)
+        
+        main_layout.addWidget(form_frame)
+        
+        # Link registrazione
+        register_layout = QHBoxLayout()
+        register_label = QLabel("Non hai un account?")
+        self.register_button = QPushButton('Registrati qui')
+        self.register_button.setObjectName('link_button')
+        self.register_button.clicked.connect(self.controller.show_register)
+        
+        register_layout.addWidget(register_label)
+        register_layout.addWidget(self.register_button)
+        register_layout.addStretch()
+        
+        main_layout.addLayout(register_layout)
+        main_layout.addStretch()
+        
+        self.setLayout(main_layout)
+        
+        # Connessione Enter per login
+        self.password_input.returnPressed.connect(self.handle_login)
 
         self.setStyleSheet("""
             QWidget {
@@ -86,71 +151,6 @@ class LoginView(QWidget):
                 background-color: #1b4f72;
             }
             """)
-    
-    def init_ui(self):
-        self.setWindowTitle('Sistema di Login')
-        #self.setFixedSize(500, 450)
-        
-        
-        # Layout principale
-        main_layout = QVBoxLayout()
-        main_layout.setSpacing(20)
-        main_layout.setContentsMargins(40, 40, 40, 40)
-        
-        # Titolo
-        title = QLabel('Accedi al tuo account')
-        title.setObjectName('title')
-        title.setAlignment(Qt.AlignCenter)
-        main_layout.addWidget(title)
-        
-        # Frame per il form
-        form_frame = QFrame()
-        form_frame.setObjectName('form_frame')
-        form_layout = QVBoxLayout(form_frame)
-        form_layout.setSpacing(15)
-        
-        # Campo username
-        username_label = QLabel('Username:')
-        self.username_input = QLineEdit()
-        self.username_input.setPlaceholderText('Inserisci il tuo username')
-        form_layout.addWidget(username_label)
-        form_layout.addWidget(self.username_input)
-        
-        # Campo password
-        password_label = QLabel('Password:')
-        self.password_input = QLineEdit()
-        self.password_input.setEchoMode(QLineEdit.Password)
-        self.password_input.setPlaceholderText('Inserisci la tua password')
-        form_layout.addWidget(password_label)
-        form_layout.addWidget(self.password_input)
-        
-        # Bottone login
-        self.login_button = QPushButton('Accedi')
-        self.login_button.setObjectName('primary_button')
-        self.login_button.clicked.connect(lambda: self.controller.handle_login(self.username_input.text(), self.password_input.text(), self))
-        #self.login_button.clicked.connect(self.handle_login)
-        form_layout.addWidget(self.login_button)
-        
-        main_layout.addWidget(form_frame)
-        
-        # Link registrazione
-        register_layout = QHBoxLayout()
-        register_label = QLabel("Non hai un account?")
-        self.register_button = QPushButton('Registrati qui')
-        self.register_button.setObjectName('link_button')
-        self.register_button.clicked.connect(self.controller.show_register)
-        
-        register_layout.addWidget(register_label)
-        register_layout.addWidget(self.register_button)
-        register_layout.addStretch()
-        
-        main_layout.addLayout(register_layout)
-        main_layout.addStretch()
-        
-        self.setLayout(main_layout)
-        
-        # Connessione Enter per login
-        self.password_input.returnPressed.connect(self.handle_login)
 
         # Centra la finestra
         self.center_window()
